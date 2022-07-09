5ce71aff034c4b40d370b3135e5eac81f757db03
iradakhf <iradakhf@code.edu.az>
2022-07-09 08:10:04 +0400

    add ConsoleApp10

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace arrays
+{
+    class Program
+    {
+        public static void Main (string [] args)
+        {
+            int[] numbers = { 1, 2, 5, 8 };
+
+            int[] reverNumbers = ReverseArray(numbers);
+            for (int i = 0; i < reverNumbers.Length; i++)
+            {
+
+                Console.WriteLine(reverNumbers[i]);
+
+            }
+            
+        }
+        public static int[] ReverseArray(int[] numbers)
+        {
+            int[] reverseArray = new int[numbers.Length]; 
+            int number = 0;
+            for (int i = numbers.Length-1, j = 0; i >=0&& j < numbers.Length; i--, j++)
+            {
+                
+                    reverseArray[j] = numbers[i];
+                
+            }
+            return reverseArray;
+        }
+    }
+}
